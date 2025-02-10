04f05ae52e919635d97190b737939530500300f6
Long Lương <robobk92@gmail.com>
2025-02-10 18:49:49 +0700

    add Comment test

--- SLTtechSoft/Program.cs
+++ SLTtechSoft/Program.cs
@@ -20,7 +20,7 @@ namespace SLTtechSoft
             Application.SetCompatibleTextRenderingDefault(false);
           
             Application.Run(new Form1());
-            
+            //Add Comment
         }
     }
 }
